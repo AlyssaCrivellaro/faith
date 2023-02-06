304f53ae353b2fe9fd2bc4401096d1ac7a5dc106
Alessandra Crivelaro <114755240+AlyssaCrivellaro@users.noreply.github.com>
2023-02-06 17:34:42 -0300

    upload
    primeiro

--- c2-back/db.js
+++ c2-back/db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "faith.cluster-ro-cag67pfpsk2z.sa-east-1.rds.amazonaws.com",
-    user: "faith",
-    password: "faith123",
-    database: "Faith"
+    host: "host",
+    user: "xxx",
+    password: "xxx",
+    database: "xxx"
 })
 
